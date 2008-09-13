c9d96440928291aafd8d3134f275d16f349c14f0
corsix <corsix@07142bc5-7355-0410-a98c-5fd2ca6e29ee>
2008-09-13 00:06:53 +0000

    Added CATCH_MESSAGE_BOX_1 exception macro Resolved potential ambiguity of pointer ownership in composition file store initialisation

--- trunk/Rainman2/exception_dialog.h
+++ trunk/Rainman2/exception_dialog.h
@@ -72,4 +72,6 @@ private:
 };
 
 #define EXCEPTION_MESSAGE_BOX(message, e) RainExceptionDialog::show(new RainException(__WFILE__, __LINE__, message, e));
+#define EXCEPTION_MESSAGE_BOX_1(message, a1, e) RainExceptionDialog::show(new RainException(__WFILE__, __LINE__, e, message, a1));
 #define CATCH_MESSAGE_BOX(message, cleanup) catch (RainException *pRainError) { EXCEPTION_MESSAGE_BOX(message, pRainError); cleanup; }
+#define CATCH_MESSAGE_BOX_1(message, a1, cleanup) catch (RainException *pRainError) { EXCEPTION_MESSAGE_BOX_1(message, a1, pRainError); cleanup; }
